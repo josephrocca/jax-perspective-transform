b7c7b4c38708f7a289d8231b5bc3ee333950216e
josephrocca <1167575+josephrocca@users.noreply.github.com>
2021-09-17 00:25:30 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jpt',
+      version='0.1',
+      description='JAX perspective transform',
+      url='https://github.com/josephrocca/jax-perspective-transform',
+      license='Apache 2.0',
+      packages=['jpt'],
+      zip_safe=False)
